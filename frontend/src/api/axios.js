92cd604d64f5442d1039576fbc675b14bea36f6b
lipakshirana36 <lipakshirana36@gmail.com>
2026-07-07 23:56:28 +0530

    fix api url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://task-manager-5-7ce5.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
